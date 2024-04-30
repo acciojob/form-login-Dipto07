b74d2605cb179a1734a9689bf7be83d4fb08cdde
Dipto Neogi <81651700+Dipto07@users.noreply.github.com>
2024-04-30 12:58:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	 const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    
+    const fullName = firstName + ' ' + lastName;
+    
+    alert(fullName);
 
 }
